3eab0877fd2b0f78d7a8005eb6ec23de9e071875
Nimishamba MD Gowda <153758807+Nimisha1610@users.noreply.github.com>
2025-07-14 18:41:48 +0530

    Create parameterMethod.java

---
+++ parameterMethod.java
@@ -0,0 +1,15 @@
+jshell> /list sayHi
+
+  35 : void sayHi(int noTimes){//PARAMETER//
+       for(int i=1;i<=noTimes;i++){
+        System.out.println("Hello world");
+       }
+       }
+
+jshell> sayHi(6)//ARGUMENT//
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
+Hello world
