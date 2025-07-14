ce1ff521a3bef9d5d35792bc3268d2e2e3fa28e7
Nimishamba MD Gowda <153758807+Nimisha1610@users.noreply.github.com>
2025-07-14 18:42:28 +0530

    Create printSquareNumbers.java

---
+++ printSquareNumbers.java
@@ -0,0 +1,18 @@
+jshell> void printSquaresOfNumbers(int n){
+   ...>     for(int i=1;i<=n;i++){
+   ...>         System.out.printf("%d",i*i).println();
+   ...>     }
+   ...> }
+|  modified method printSquaresOfNumbers(int)
+
+jshell> printSquaresOfNumbers(10)
+1
+4
+9
+16
+25
+36
+49
+64
+81
+100
