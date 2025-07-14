81096764cac6e374770a15500d1f21350e78e46d
Nimishamba MD Gowda <153758807+Nimisha1610@users.noreply.github.com>
2025-07-14 18:45:01 +0530

    Create methodOverloading.java

---
+++ methodOverloading.java
@@ -0,0 +1,18 @@
+METHOD OVERLOADING 
+
+
+void mul(){           //NO ARGUMENT TO ACCEPT PARAMETER
+    for(int i=1;i<10;i++){
+        System.out.printf("%d*%d=%d",i,5,i*5).println();
+    }
+}
+void mul(int num){      //ARGUMENT TO ACCEPT PARAMETER
+    for(int i=1;i<10;i++){
+        System.out.printf("%d*%d=%d",i,num,i*num).println();
+    }
+}
+
+*calling method with parameter and calling method without parameter
+>>creating two different methods with the same name with different parameter
+>>two diff methods doing similar thing giving same name ,diff is parameter
+
