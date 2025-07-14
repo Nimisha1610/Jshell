27c25d13cb88e6a471e854cd544ae8460834c3a8
Nimishamba MD Gowda <153758807+Nimisha1610@users.noreply.github.com>
2025-07-14 18:44:01 +0530

    Create methodMultiplication.java

---
+++ methodMultiplication.java
@@ -0,0 +1,19 @@
+jshell> void printMultiplicationTable(int num)
+   ...> {
+   ...>     for(i=1;i<=10;i++)
+   ...> {
+   ...>     System.out.printf("%d*%d=%d",i,num,i*num).println();
+   ...> }}
+|  modified method printMultiplicationTable(int)
+
+jshell> printMultiplicationTable(8)
+1*8=8
+2*8=16
+3*8=24
+4*8=32
+5*8=40
+6*8=48
+7*8=56
+8*8=64
+9*8=72
+10*8=80
