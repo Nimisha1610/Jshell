51876059f5bac33f0fe70902621e791d55e01456
Nimishamba MD Gowda <153758807+Nimisha1610@users.noreply.github.com>
2025-07-14 18:39:56 +0530

    Create javaMethod.java

---
+++ javaMethod.java
@@ -0,0 +1,13 @@
+ReturnType nameOfMethod(){
+//WHAT TO DO
+}
+
+ jshell> void sayHeloWorldTwice(){
+   ...>     System.out.println("HELLO WORLD");
+   ...>     System.out.println("HEllo world");
+   ...> }//creating a method
+|  created method sayHeloWorldTwice()
+
+jshell> sayHeloWorldTwice()//methodname smallletters
+HELLO WORLD
+HEllo world
